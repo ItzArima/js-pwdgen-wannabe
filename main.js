4c205f19ccd7d0c0d46e5cb30caa1d83278fd3e7
ItzArima <skaypaprilia@gmail.com>
2021-10-11 10:49:04 +0200

    impostato output più pertinente alla richiesta dell'esercizio

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 var name = window.prompt("Qual'è il tuo nome?");
 var surname = window.prompt("Qual'è il tuo cognome?");
 var color = window.prompt("Qual'è il tuo colore preferito?")
-document.write("Ciao " + name + " " + surname + " ," + " il tuo colore preferito è il: " + color + " 21");
+document.write("Ciao " + name + " " + surname + " ," + " la tua password è: " + name + surname + color + "21");
 
